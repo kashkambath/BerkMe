eb3ab771b04a7c7067b294f59ce6c89b4d8cc6d1
Varun Bhatia <varunbhatia@Varuns-MacBook-Pro-2.local>
2017-10-07 21:39:36 -0700

    Added maps

--- FlaskApp/static/script.js
+++ FlaskApp/static/script.js
@@ -1,6 +1,7 @@
 var map;
 
 var filter;
+var markers = [];
 function initMap() {
   var uluru = {lat: 37.8716, lng: -122.2727};
   map = new google.maps.Map(document.getElementById('map'), {
@@ -12,12 +13,15 @@ function initMap() {
       position: uluru,
       map: map
   });
+  markers.push(marker);
   var geocoder = new google.maps.Geocoder();
 
   document.getElementById('searchbtn').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
   });
 }
+
+
 
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
@@ -28,6 +32,10 @@ function geocodeAddress(geocoder, resultsMap) {
         map: resultsMap,
         position: results[0].geometry.location
       });
+      markers[0].setMap(null);
+      markers = []
+      markers.push(marker);
+      console.log("Marker 2");
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
@@ -37,3 +45,95 @@ function geocodeAddress(geocoder, resultsMap) {
 function changeFilter(value) {
   document.getElementById('filter').textContent = value;
 }
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 1; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+// $("#map").change(function() {
+//   initMap();
+// });
+// //var geocoder;
+// var map;
+// function initMap() {
+//   //geocoder = new google.maps.Geocoder();
+//   var uluru = {lat: 37.8716, lng: -122.2727};
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 14,
+//     center: uluru,
+//     mapTypeId: 'roadmap'
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
+//
+// // $("#searchbtn").click(function() {
+// //   console.log("Hi");
+// //   codeAddress();
+// // });
+//
+// // function codeAddress() {
+// // var addressInput = document.getElementById('address').value;
+// // $.ajax({
+// //     url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCmlmB_d0ILG7qO3bZ9e-isQGR25gvWBBM',
+// //     type : 'GET',
+// //     data : {
+// //         
+// //     },
+// //     dataType:'json',
+// //     success : function(data) {              
+// //         console.log('Data: '+data);
+// //     },
+// //     error : function(request,error)
+// //     {
+// //         console.log("Request: "+JSON.stringify(request));
+// //     }
+// // });
+// // // var geocoder = new google.maps.Geocoder();
+// // //
+// // // geocoder.geocode({address: addressInput}, function(results, status) {
+// // //   console.log(addressInput);
+// // //   if (status == google.maps.GeocoderStatus.OK) {
+// // //     map.setCenter(results[0].geometry.location);
+// // //     var marker = new google.maps.Marker({
+// // //         map: map,
+// // //         position: results[0].geometry.location
+// // //     });
+// // //   } else {
+// // //     alert("Geocode was not successful for the following reason: " + status);
+// // //   }
+// // // });
+// // }
+//
+// // function codeAddress() {
+// //     var address = document.getElementById('address').value;
+// //     console.log(address);
+// //     geocoder.geocode( {'address': address}, function(results, status) {
+// //       if (status == 'OK') {
+// //         map.setCenter(results[0].geometry.location);
+// //         var marker = new google.maps.Marker({
+// //             map: map,
+// //             position: results[0].geometry.location
+// //         });
+// //       } else {
+// //         alert('Geocode was not successful for the following reason: ' + status);
+// //       }
+// //     });
+// //   }
+//
+// // function changeMap() {
+// //
+// // }
